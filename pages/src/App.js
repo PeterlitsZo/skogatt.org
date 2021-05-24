60f64a5e70eed67c55a3d53a37bf1228f9e3f0df
Peterlits Zo <peterlitszo@outlook.com>
2021-05-24 17:00:23 +0800

    feat: change the home page from the default page.
    The default page is brought by `create-react-app`.
    
    I change it to be my home page.

--- pages/src/App.js
+++ pages/src/App.js
@@ -1,15 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        Peterlits
+        <div className="App-header-footnote">
+          <p className="App-header-footnote-font">生活 · 技术 · 存在 · 我</p>
+          <p className="App-header-footnote-font App-header-footnote-none" />
+          <p className="App-header-footnote-font">●</p>
+        </div>
       </header>
+      <div className="App-content">
+        <p>这里是 Peterlits 的小网站。Peterlits 其人，本名周泓余，于苏州大学计算机科学与技术系在读。</p>
+      </div>
     </div>
   );
 }
