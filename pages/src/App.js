83609b78c2ad029927f24e49b0ed2fcbed502190
Peterlits Zo <peterlitszo@outlook.com>
2021-05-27 23:50:18 +0800

    refa: split components from the `App.js`
    There are three components in a single file. Now we split it into three
    file (`App.js`, `components/LikeButton.js`, `components/DislikeButton.js`).

--- pages/src/App.js
+++ pages/src/App.js
@@ -1,89 +1,9 @@
 import React from 'react';
-import axios from 'axios';
 
 import './App.scss';
-import {ReactComponent as Like} from './svg/thumbs-up.svg'
-import {ReactComponent as Dislike} from './svg/thumbs-down.svg'
 
-
-class LikeButtom extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {like_number: NaN};
-
-    this.addOne = this.addOne.bind(this);
-    axios.get('/api/v1/home/like')
-      .then(response => {
-        console.log(response);
-        this.setState({
-          like_number: response.data
-        });
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }
-  addOne() {
-    this.setState(state => {
-      if (isNaN(state.like_number)) {
-        return {}
-      } else {
-        axios.post('/api/v1/home/like');
-        return {
-          like_number: state.like_number + 1
-        }
-      }
-    });
-  }
-  render() {
-    return (
-      <div className="buttom" onClick={this.addOne}>
-        <Like className="icon" />
-        Like({isNaN(this.state.like_number) ? '...' : this.state.like_number})
-      </div>
-    );
-  }
-}
-
-
-class DislikeButtom extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {dislike_number: NaN};
-
-    this.addOne = this.addOne.bind(this);
-    axios.get('/api/v1/home/dislike')
-      .then(response => {
-        console.log(response);
-        this.setState({
-          dislike_number: response.data
-        });
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }
-  addOne() {
-    this.setState(state => {
-      if (isNaN(state.dislike_number)) {
-        return {}
-      } else {
-        axios.post('/api/v1/home/dislike');
-        return {
-          dislike_number: state.dislike_number + 1
-        }
-      }
-    });
-  }
-  render() {
-    return (
-      <div className="buttom" onClick={this.addOne}>
-        <Dislike className="icon" />
-        Disike({isNaN(this.state.dislike_number) ? '...' : this.state.dislike_number})
-      </div>
-    );
-  }
-}
+import LikeButtom from './components/LikeButton'
+import DislikeButtom from './components/DislikeButton'
 
 function App() {
   return (
